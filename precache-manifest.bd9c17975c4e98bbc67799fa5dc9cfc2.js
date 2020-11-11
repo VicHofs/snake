78920adae7b8c2a200543dbdc335d2630cae32f8
VicHofs <57238498+VicHofs@users.noreply.github.com>
2020-11-11 15:02:47 -0300

    Updates

---
+++ precache-manifest.bd9c17975c4e98bbc67799fa5dc9cfc2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863092795fab305a5280a566607ee5d5",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "cc9bc0c0989e2d65c47d",
+    "url": "/snake/static/css/main.c59b66c6.chunk.css"
+  },
+  {
+    "revision": "2c9b5a3b45cf1914c182",
+    "url": "/snake/static/js/2.14cc569b.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/snake/static/js/2.14cc569b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9bc0c0989e2d65c47d",
+    "url": "/snake/static/js/main.f2a6f2a9.chunk.js"
+  },
+  {
+    "revision": "fab9d91fecf0babc8498",
+    "url": "/snake/static/js/runtime-main.608e748d.js"
+  },
+  {
+    "revision": "fd6111496ceefa7c313978248a28c59a",
+    "url": "/snake/static/media/gameOver.fd611149.mp3"
+  },
+  {
+    "revision": "f499fc89edc4426ed4167ae6c87fb997",
+    "url": "/snake/static/media/hit.f499fc89.wav"
+  },
+  {
+    "revision": "55128ad6629de15adcf86b10ac9e7423",
+    "url": "/snake/static/media/point.55128ad6.wav"
+  }
+]);
